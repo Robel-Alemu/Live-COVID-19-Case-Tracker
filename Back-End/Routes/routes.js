f0015e97e9fafd6b34ca5257e008c2543c66c65c
Robel <robelalemu2372@gmail.com>
2022-08-17 17:37:00 +0300

    australia and europe data done

--- Back-End/Routes/routes.js
+++ Back-End/Routes/routes.js
@@ -12,12 +12,22 @@ const {
     AfricaData
 } = require("../Controller/ContinentController/AfircaDataController");
 
+const {
+    AustraliaAndOceanianData
+} = require("../Controller/ContinentController/AustraliaAndOceanianController");
+
+const {
+    EuropeData
+} = require("../Controller/ContinentController/EuropeDataController");
+
 
 const router = express.Router(); 
 
 router.get("/world-data", WorldData);
 router.get("/asia-data", AllAsiaData);
 router.get("/africa-data", AfricaData);
+router.get("/australia-data", AustraliaAndOceanianData);
+router.get("/europe-data", EuropeData);
 
 
 module.exports = {
